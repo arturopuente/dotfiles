97e7e4585984da9aff2d4c0cc36f635506302677
Arturo Puente <arturo@able.co>
2017-06-26 17:19:58 -0500

    Prefer local executables before gem executables

--- fish/env.fish
+++ fish/env.fish
@@ -4,16 +4,15 @@ set PATH $HOME/bin $PATH
 set EDITOR vim
 
 # Rbenv bin and shims of the current ruby
-set PATH ~/.rbenv/bin $PATH
-set PATH ~/.rbenv/shims $PATH
+set PATH $PATH ~/.rbenv/bin
+set PATH $PATH ~/.rbenv/shims
 
 # nvm
-
 export NVM_DIR="$HOME/.nvm"
-# . "/usr/local/opt/nvm/nvm.sh"
 
 # Rehash rbenv to get the latest rubies installed
 rbenv rehash >/dev/null ^&1
 
 set fish_greeting ""
 set fish_color_cwd "bbbbbb"
+
