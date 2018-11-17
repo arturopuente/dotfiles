6c01d53ad29421befbabdd60ec53c34a5e963aae
Arturo Puente <arturo@able.co>
2018-11-16 20:59:12 -0500

    Switch from rbenv to chruby

--- fish/env.fish
+++ fish/env.fish
@@ -3,15 +3,12 @@ set PATH "/usr/local/bin" $PATH
 set PATH $HOME/bin $PATH
 set EDITOR vim
 
-# Rbenv bin and shims of the current ruby
-set PATH ~/.rbenv/bin $PATH
-set PATH ~/.rbenv/shims $PATH
+# chruby config
+source /usr/local/share/chruby/chruby.fish
+source /usr/local/share/chruby/auto.fish
 
 # nvm
 export NVM_DIR="$HOME/.nvm"
-
-# Rehash rbenv to get the latest rubies installed
-rbenv rehash >/dev/null ^&1
 
 set fish_greeting ""
 set fish_color_cwd "bbbbbb"
