a4c282d473965445d68ebd6cb9f55f0f066ec087
Arturo Puente <arturo@able.co>
2018-12-10 23:05:43 -0500

    Updates emacs config with prettier, maximized windows and a custom backups dir

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,11 +19,21 @@
 (evil-mode 1)
 
 (setq explicit-shell-file-name "/usr/local/bin/fish")
+(setq vc-follow-symlinks t)
 
 (tool-bar-mode -1) 
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 (global-linum-mode t)
 (setq inhibit-startup-screen t)
 
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
+
+(require 'prettier-js)
+(add-hook 'js2-mode-hook 'prettier-js-mode)
+(add-hook 'rjsx-mode-hook 'prettier-js-mode)
+(add-hook 'web-mode-hook 'prettier-js-mode)
+
+(setq backup-directory-alist
+      `(("." . ,(concat user-emacs-directory "backups"))))
