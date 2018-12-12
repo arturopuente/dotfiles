62951d96660b226a7328e4ae2a42f0ca98265a44
Arturo Puente <arturo@able.co>
2018-12-11 22:05:54 -0500

    Add comments to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,32 +11,42 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
 
-(load-theme 'rebecca t)
-(set-face-attribute 'default nil
-  :family "Monaco" :height 230 :weight 'normal)
-
-(require 'evil)
-(evil-mode 1)
-
-(setq explicit-shell-file-name "/usr/local/bin/fish")
-(setq vc-follow-symlinks t)
-
-(tool-bar-mode -1) 
+;; default window options
+(tool-bar-mode -1)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (global-linum-mode t)
 (setq column-number-mode t)
 (setq inhibit-startup-screen t)
 
+;; visual options
+(load-theme 'rebecca t)
+(set-face-attribute 'default nil
+  :family "Monaco" :height 230 :weight 'normal)
+
+;; best vim emulation mode ever
+(require 'evil)
+(evil-mode 1)
+
+;; sets fish as the command for ansi-term
+(setq explicit-shell-file-name "/usr/local/bin/fish")
+
+;; ~/.emacs.d/init.el is a symlink
+(setq vc-follow-symlinks t)
+
+;; enable C-x C-f fuzzy finding
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
 
+;; move with shift + directional arrows
 (windmove-default-keybindings)
 
+;; hook prettier to run in the major web modes
 (require 'prettier-js)
 (add-hook 'js2-mode-hook 'prettier-js-mode)
 (add-hook 'rjsx-mode-hook 'prettier-js-mode)
 (add-hook 'web-mode-hook 'prettier-js-mode)
 
+;; avoid cluttering up the directories with backup files
 (setq backup-directory-alist
       `(("." . ,(concat user-emacs-directory "backups"))))
