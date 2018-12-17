b2c6a728448145f6230b7512d2f46b08b1b5a7ae
Arturo Puente <arturo@able.co>
2018-12-17 09:46:03 -0500

    Configure swiper for file search

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -72,3 +72,36 @@
 (use-package flyspell
   :hook ((text-mode . flyspell-mode)
          (prog-mode . flyspell-prog-mode)))
+
+(use-package swiper
+	:ensure t
+	:config
+	(progn
+		(ivy-mode 1)
+		(setq ivy-use-virtual-buffers t)
+		(setq enable-recursive-minibuffers t)
+		(global-set-key "\C-s" 'swiper)
+		(global-set-key (kbd "C-c C-r") 'ivy-resume)
+		(global-set-key (kbd "<f6>") 'ivy-resume)
+		(global-set-key (kbd "M-x") 'counsel-M-x)
+		(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+		(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+		(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+		(global-set-key (kbd "<f1> l") 'counsel-find-library)
+		(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+		(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+		(global-set-key (kbd "C-c g") 'counsel-git)
+		(global-set-key (kbd "C-c j") 'counsel-git-grep)
+		(global-set-key (kbd "C-c k") 'counsel-ag)
+		(global-set-key (kbd "C-x l") 'counsel-locate)
+		(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+		(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+	))
+
+(define-key ivy-minibuffer-map (kbd "C-h") (kbd "DEL"))
+(define-key ivy-minibuffer-map (kbd "C-j") 'ivy-previous-line)
+(define-key ivy-minibuffer-map (kbd "C-k") 'ivy-next-line)
+(define-key ivy-minibuffer-map (kbd "C-l") 'ivy-alt-done)
+(define-key ivy-minibuffer-map (kbd "<return>") 'ivy-alt-done)
+(define-key ivy-minibuffer-map (kbd "C-:") 'ivy-dired)
+(define-key ivy-minibuffer-map (kbd "C-c o") 'ivy-occur)
