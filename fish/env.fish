aa938b8949ae89e4c1fe4c4a454b9bcb50c1b30a
Arturo Puente <arturo@able.co>
2018-12-23 15:39:51 -0500

    Remove outdated nvm config

--- fish/env.fish
+++ fish/env.fish
@@ -6,8 +6,5 @@ set EDITOR vim
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
 
-# nvm
-export NVM_DIR="$HOME/.nvm"
-
 set fish_greeting ""
 set fish_color_cwd "bbbbbb"
