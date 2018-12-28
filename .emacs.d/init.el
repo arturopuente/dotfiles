bb237171437d2dba581d75641100081f69db3716
Arturo Puente <arturo@able.co>
2018-12-28 03:26:35 -0500

    Declare web-mode config via use-package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,13 +58,17 @@
 (setq-default tab-width 2)
 (setq js-indent-level 2)
 
-(defun arturo/web-mode-hook ()
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
-)
+(use-package web-mode
+  :ensure t
+  :init (setq web-mode-markup-indent-offset 2)
+        (setq web-mode-code-indent-offset 2)
+        (setq web-mode-css-indent-offset 2)
+        (setq web-mode-enable-auto-pairing t)
+        (setq web-mode-enable-auto-expanding t)
+        (setq web-mode-enable-css-colorization t))
 
-(add-hook 'web-mode-hook  'arturo/web-mode-hook)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 
 ;; enable (relative) line numbers
 (global-linum-mode t)
