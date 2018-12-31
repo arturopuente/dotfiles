1b70ba742397b70909b44a3b5b21afc073f1b08a
Arturo Puente <arturo@able.co>
2018-12-30 21:11:17 -0500

    Add company-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -191,6 +191,12 @@
 
 (add-hook 'rjsx-mode-hook 'electric-pair-mode)
 
+(use-package company
+  :ensure t
+  :pin melpa)
+
+(add-hook 'after-init-hook 'global-company-mode)
+
 (use-package inf-ruby
   :ensure t)
 
