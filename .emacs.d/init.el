71d6f6e5e9da1e10942542fcbec3074b9ac0ff98
Arturo Puente <arturo@able.co>
2019-04-14 18:31:06 -0500

    Initialize package manager

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,8 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
 
+(package-initialize)
+
 (require 'use-package)
 
 (use-package exec-path-from-shell
