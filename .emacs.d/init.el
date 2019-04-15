5153a8a28df36083ec5321528fa3b3cb34e391b9
Arturo Puente <arturo@able.co>
2019-04-14 21:56:52 -0500

    Change neotree-refresh key binding to r

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -441,7 +441,7 @@ of the block."
 (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
 (evil-define-key 'normal neotree-mode-map (kbd "o") 'neotree-quick-look)
 (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
-(evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
+(evil-define-key 'normal neotree-mode-map (kbd "r") 'neotree-refresh)
 (evil-define-key 'normal neotree-mode-map (kbd "j") 'neotree-next-line)
 (evil-define-key 'normal neotree-mode-map (kbd "k") 'neotree-previous-line)
 (evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
