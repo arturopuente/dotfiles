19dd71b4ed7c97a9a2f6f977bd190cce9094a50f
Arturo Puente <arturo@able.co>
2019-04-16 00:48:31 -0500

    Load snippets in org-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -352,6 +352,7 @@
 
 (yas-reload-all)
 (add-hook 'prog-mode-hook #'yas-minor-mode)
+(add-hook 'org-mode-hook #'yas-minor-mode)
 
 (defun my-delete-word (arg)
   "Delete characters forward until encountering the end of a word.
