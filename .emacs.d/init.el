8f7ffbcb57eb2a9a48d731254025dd269b0cfc58
Arturo Puente <arturo@able.co>
2019-05-30 02:10:01 -0500

    Remove custom dired sorting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -37,19 +37,6 @@
 (when (eq system-type 'darwin)
   (setq dired-use-ls-dired nil))
 
-(defun mydired-sort ()
-  "Sort dired listings with directories first."
-  (save-excursion
-    (let (buffer-read-only)
-      (forward-line 2) ;; beyond dir. header
-      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
-    (set-buffer-modified-p nil)))
-
-(defadvice dired-readin
-  (after dired-after-updating-hook first () activate)
-  "Sort dired listings with directories first before adding marks."
-  (mydired-sort))
-
 (add-hook 'dired-mode-hook
   (lambda ()
     (dired-hide-details-mode)))
