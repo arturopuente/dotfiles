122057de41845223b248ea86484e7e59b23cc618
Arturo Puente <arturo@able.co>
2019-06-22 12:20:12 -0500

    Move Org Babel into its own section

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -317,15 +317,29 @@ This command does not push text to `kill-ring'."
 
 (setq initial-major-mode 'org-mode)
 
+(add-hook 'org-mode-hook
+          (lambda () (setq evil-auto-indent nil)))
+
+(use-package ob-restclient
+  :ensure t)
+
 (with-eval-after-load 'org
   (org-babel-do-load-languages 'org-babel-load-languages
-                              '((ruby . t)
-                                (js . t))))
+                                '((ruby . t)
+                                  (calc . t)
+                                  (ruby . t)
+                                  (lisp . t)
+                                  (shell . t)
+                                  (restclient . t)
+                                  (js . t))))
 
 (setq org-confirm-babel-evaluate nil)
 
-(add-hook 'org-mode-hook
-          (lambda () (setq evil-auto-indent nil)))
+(setq org-src-fontify-natively t
+      org-src-window-setup 'current-window
+      org-src-strip-leading-and-trailing-blank-lines t
+      org-src-preserve-indentation t
+      org-src-tab-acts-natively t)
 
 (use-package magit
   :ensure t)
